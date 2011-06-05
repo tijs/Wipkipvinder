b647bbb367fd6bce56643d15c4582e1d2ed8e2ee
Tijs Teulings <tteulings@gmail.com>
2011-06-05 21:29:26 +0200

    moving stuff into templates and doing some cleaning up, setup basic caching for main API call

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from api.handlers import PlaythingsHandler
-#from django.views.decorators.cache import cache_control
+from django.views.decorators.cache import cache_control
 
-#cached_resource = cache_control(static_root=True, maxage=30, s_maxage=300) # cache resource (when enabled)
-play_resource = Resource(handler=PlaythingsHandler)
+cached_resource = cache_control(static_root=True, maxage=300, s_maxage=3000) # cache resource (when enabled)
+play_resource = cached_resource(Resource(handler=PlaythingsHandler))
 
 urlpatterns = patterns('',
     #url(r'^all\.(?P<emitter_format>.+)$', play_handler),
